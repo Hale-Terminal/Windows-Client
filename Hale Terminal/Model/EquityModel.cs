ff3ea3aae8fac731289f8c3a379dca8e877c26b4
Andrew Hale <halea2196@gmail.com>
2022-08-28 12:16:33 -0400

    Add project files.

---
+++ Hale Terminal/Model/EquityModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hale_Terminal.Model
+{
+    class EquityModel
+    {
+        public string Ticker { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CurrentPrice { get; set; }
+        public string PriceChange { get; set; }
+        public string LastBid { get; set; }
+        public string LastBuy { get; set; }
+        public string Volume { get; set; }
+        public string OpenPrice { get; set; }
+        public string HighPrice { get; set; }
+        public string LowPrice { get; set; }
+        public string Valuation { get; set; }
+        public string PriceChangeDay { get; set; }
+        public string YearWeekHigh { get; set; }
+        public string YearWeekLow { get; set; }
+        public string YTDChange { get; set; }
+        public string MktCap { get; set; }
+        public string SharesOutstanding { get; set; }
+        public string ShortInterest { get; set; }
+        public string DaysToCover { get; set; }
+        public string Date { get; set; }
+        public string PE { get; set; }
+        public string EstPE { get; set; }
+        public string TYTDMEPS { get; set; }
+        public string EstEPS { get; set; }
+        public string EstPEG { get; set; }
+        public string IndGrossYield { get; set; }
+        public string Website { get; set; }
+        public string HQ { get; set; }
+        public string EmployeeCount { get; set; }
+        public string President { get; set; }
+        public string VisePresident { get; set; }
+        public string SecondVP { get; set; }
+        public string TotRet { get; set; }
+        public string BetaVsSPX { get; set; }
+    }
+}
---
+++ Hale Terminal/ViewModel/EquityViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Hale_Terminal.Model;
+using Hale_Terminal.Core;
+
+namespace Hale_Terminal.ViewModel
+{
+    class EquityViewModel : ObservableObject
+    { 
+
+        public EquityModel Equity { get; set; }
+
+        public EquityViewModel()
+        {
+            Equity = new EquityModel{
+                Ticker = "BA US",
+                Name = "BOEING CO/THE",
+                Description = "The Boeing Company, together with its subsidiaries, develops, produces, and markets commercial jet aircraft, as well as provides related support services to the commercial airline industry worldwide. The Company also researches, develops, produces, modifies, and supports information, space, and defense systems, including military aircraft",
+                CurrentPrice = "131.46",
+                PriceChange = "-1.91",
+                LastBid = "131.38",
+                LastBuy = "131.39",
+                Volume = "30,702,692",
+                OpenPrice = "127.115",
+                HighPrice = "131.50",
+                LowPrice = "124.35",
+                Valuation = "4.272B",
+                PriceChangeDay = "131.46/-1.43%",
+                YearWeekHigh = "391.00",
+                YearWeekLow = "89.00",
+                YTDChange = "-194.30/-59.65%",
+                MktCap = "74,186.2M",
+                SharesOutstanding = "564.3M/532.7M",
+                ShortInterest = "14.6M/2.74%",
+                DaysToCover = "0.4",
+                Date = "07/24/20",
+                PE = "N.A.",
+                EstPE = "N.A.",
+                TYTDMEPS = "-8.56",
+                EstEPS = "-2.98",
+                EstPEG = "N.A.",
+                IndGrossYield = "N.A.",
+                Website = "www.boeing.com",
+                HQ = "Chicago, IL, US",
+                EmployeeCount = "161,100 (12/31/19)",
+                President = "David L Calhoun - Dave",
+                VisePresident = "Leanne G Caret",
+                SecondVP = "Theodore Colbert III - Ted",
+                TotRet = "-64.25%",
+                BetaVsSPX = "1.97"
+            };
+            
+        }
+    }
+}
